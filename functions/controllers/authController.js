e9b8e62ef334705068831adc0f36976b17dcc4a6
Aum Zaveri <aumzaveri06@gmail.com>
2025-10-07 15:50:57 -0400

    is user verified, implemented

--- functions/controllers/authController.js
+++ functions/controllers/authController.js
@@ -1,3 +1,4 @@
+const { disableNetwork } = require('firebase/firestore')
 const authService = require('../services/authService')
 const asyncErrorHandler = require('../utils/asyncErrorHandler')
 
@@ -6,6 +7,17 @@ const authController = {
         await authService.signUp(req.body)
 
         res.status(201).json({})
+    }),
+
+    ifUserVerified : asyncErrorHandler(async (req, res, next) => {
+        const response = await authService.ifUserVerified(req.body)
+
+        res.status(200).json({
+            exists: response.exists,
+            emailVerified: response.emailVerified,
+            phoneVerified: response.phoneVerified,
+            disabled: response.disabled
+        })
     })
 }
 
--- functions/routes/authRoutes.js
+++ functions/routes/authRoutes.js
@@ -3,5 +3,6 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/sign-up", authController.signUp);
+router.post("/if-user-exists", authController.ifUserVerified);
 
 module.exports = router;
